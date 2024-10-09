11527c3fc338ed02dea25d272dc7ce6dcaf67c80
mohiiit <mohit18343@iiitd.ac.in>
2024-10-10 00:10:49 +0530

    testing madara with devnet success

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use starknet::{
     accounts::{Account, ExecutionEncoding, SingleOwnerAccount},
     core::{
-        chain_id,
-        types::{BlockId, BlockTag, Call, Felt},
+        types::{BlockId, BlockTag, Call, Felt, TransactionReceiptWithBlockInfo},
         utils::get_selector_from_name,
     },
     macros::felt,
@@ -13,8 +12,36 @@ use starknet::{
     },
     signers::{LocalWallet, SigningKey},
 };
-use starknet_api::core::{ChainId, ContractAddress, PatriciaKey};
 use tokio;
+use std::future::Future;
+
+pub async fn assert_poll<F, Fut>(f: F, polling_time_ms: u64, max_poll_count: u32)
+where
+    F: Fn() -> Fut,
+    Fut: Future<Output = bool>,
+{
+    for _poll_count in 0..max_poll_count {
+        if f().await {
+            return; 
+        }
+
+        tokio::time::sleep(tokio::time::Duration::from_millis(polling_time_ms)).await;
+    }
+
+    panic!("Max poll count exceeded.");
+}
+
+pub async fn get_transaction_receipt(
+    rpc: &JsonRpcClient<HttpTransport>,
+    transaction_hash: Felt,
+) -> TransactionReceiptWithBlockInfo {
+    // there is a delay between the transaction being available at the client
+    // and the sealing of the block, hence sleeping for 500ms
+    assert_poll(|| async { rpc.get_transaction_receipt(transaction_hash).await.is_ok() }, 500, 20).await;
+
+    rpc.get_transaction_receipt(transaction_hash).await.unwrap()
+}
+
 
 #[tokio::main]
 async fn main() {
@@ -22,10 +49,14 @@ async fn main() {
         Url::parse("http://localhost:9944").unwrap(),
     ));
 
+    let provider2 = JsonRpcClient::new(HttpTransport::new(
+        Url::parse("http://localhost:9944").unwrap(),
+    ));
+
     let signer = LocalWallet::from(SigningKey::from_secret_scalar(
         Felt::from_hex("0x76f2ccdb23f29bc7b69278e947c01c6160a31cf02c19d06d0f6e5ab1d768b86").unwrap(),
     ));
-    let address = Felt::from_hex("0x5e9e93c6235f8ae6c2f4f0069bd30753ec21b26fbad80cfbf5da2c1bc573d69").unwrap();
+    let address = Felt::from_hex("0x3bb306a004034dba19e6cf7b161e7a4fef64bc1078419e8ad1876192f0b8cd1").unwrap();
     let eth_token_address =
         Felt::from_hex("0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7")
             .unwrap();
@@ -40,8 +71,6 @@ async fn main() {
         ExecutionEncoding::New,
     );
 
-    // `SingleOwnerAccount` defaults to checking nonce and estimating fees against the latest
-    // block. Optionally change the target block to pending with the following line:
     account.set_block_id(BlockId::Tag(BlockTag::Pending));
 
     let result = account
@@ -55,4 +84,8 @@ async fn main() {
         .unwrap();
 
     println!("Transaction hash: {:#064x}", result.transaction_hash);
+
+    let receipt = get_transaction_receipt(&provider2, result.transaction_hash).await;
+    println!("Transaction receipt: {:?}", receipt);
+
 }
